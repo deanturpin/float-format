f34506e7d252edf4ca93e8ed70495a8bd1addb22
deanturpin <git@lvbtn.uk>
2017-09-12 21:44:13 +0100

    Add range vector

--- float_format.cpp
+++ float_format.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <bitset>
+#include <vector>
 
 int main() {
 
   using namespace std;
 
-  for (float f = 1.0; f < 4000.0; f += .1) {
+  const vector<vector<float>> ranges {
+    {1.0, 100, 1.1},
+    {3000.0, 3500.0, .1}
+  };
 
-    // Dirty cast rather than truncation to long
-    const auto bin = *reinterpret_cast<unsigned long*>(&f);
+  for (const auto &r : ranges) {
 
-    // Print the hex and the binary
-    cout << "0x" << hex << bin << dec << " " << bitset<32>(bin) << " " << f << endl;
+    cout << "# " << r.at(0) << " to " << r.at(1) << endl;
+    cout << "```" << endl;
+
+    for (float f = r.at(0); f < r.at(1); f += r.at(2)) {
+
+      // Dirty cast rather than truncation to long
+      const auto bin = *reinterpret_cast<unsigned long*>(&f);
+
+      // Print the hex and the binary
+      cout << "0x" << hex << bin << dec << " " << bitset<32>(bin) << " " << f << endl;
+    }
+
+    cout << "```" << endl;
   }
 
   return 0;
--- makefile
+++ makefile
@@ -1,11 +1,12 @@
 readme.md: float_format.o
 	cat _readme.md > readme.md
-	echo '```' >> readme.md
 	./float_format.o >> readme.md
-	echo '```' >> readme.md
 
 %.o:%.cpp
 	clang++ -Wno-c++98-compat -Weverything -o $@ $< -std=c++14
 
 clean:
 	rm -f float_format.o readme.md
+
+wait:
+	while :; do inotifywait -qe modify *.cpp; make -j; done
