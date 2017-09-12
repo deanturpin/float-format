f3219b69fcddf525dd665558bc37219d9b9d8d87
deanturpin <git@lvbtn.uk>
2017-09-12 21:57:00 +0100

    Add another range and use bitset to_ulong

--- float_format.cpp
+++ float_format.cpp
@@ -7,7 +7,8 @@ int main() {
   using namespace std;
 
   // Define some ranges of interest: start, end, increment
-  const vector<vector<float>> ranges{{1.0, 100, 1.1}, {3000.0, 3500.0, .1}};
+  const vector<vector<float>> ranges{
+      {.01f, 0.2f, .01f}, {1.0f, 100.0f, 1.1f}, {3000.0f, 3500.0f, .1f}};
 
   for (const auto &r : ranges) {
 
@@ -17,12 +18,13 @@ int main() {
 
     for (float f = r.at(0); f < r.at(1); f += r.at(2)) {
 
-      // Dirty cast rather than truncation to long
+      // Dirty cast rather than truncate
       const auto bin = *reinterpret_cast<unsigned long *>(&f);
 
       // Print the hex and the binary
-      cout << "0x" << hex << bin << dec << " ";
-      cout << bitset<32>(bin) << " " << f << endl;
+      const bitset<32> bits(bin);
+      cout << "0x" << hex << bits.to_ulong() << dec << " ";
+      cout << bits << " " << f << endl;
     }
 
     cout << "```" << endl;
